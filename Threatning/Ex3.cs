b13d43c640b0be97dc7266cecc21112c28a2cc25
Butyricoil <butyricoil@gmail.com>
2025-01-05 22:55:54 +0400

    add 4exercise

--- Threatning/Ex3.cs
+++ Threatning/Ex3.cs
@@ -1,25 +1,25 @@
-﻿namespace csharp_project_begin.Threatning;
-
-using System.Threading.Tasks;
-using System.Threading;
-
-
-public class Ex3
-{
-    public static Task Main(string[] args)
-    {
-        Task task1 = Task.Run(() => PrintMessage("Task 1"))};
-        Task task2 = Task.Run(() => PrintMessage("Task 2"))};
-
-        await Task.WhenAll(task1, task2);
-    }
-
-    static void PrintMessage(string message)\
-    {
-        for (int i = 0; i < 5; i++)
-        {
-           Console.WriteLine($"{message}: {i}");
-           Task.Delay(500).Wait();
-        }
-    }
-}
+﻿// namespace csharp_project_begin.Threatning
+// {
+//     using System.Threading.Tasks;
+//     using System.Threading;
+//
+//     public class Ex3
+//     {
+//         public static async Task Main(string[] args)
+//         {
+//             Task task1 = Task.Run(() => PrintMessage("Task 1"));
+//             Task task2 = Task.Run(() => PrintMessage("Task 2"));
+//
+//             await Task.WhenAll(task1, task2);
+//         }
+//
+//         static void PrintMessage(string message)
+//         {
+//             for (int i = 0; i < 5; i++)
+//             {
+//                 Console.WriteLine($"{message}: {i}");
+//                 Task.Delay(500).Wait();
+//             }
+//         }
+//     }
+// }
---
+++ Threatning/Ex4.cs
@@ -0,0 +1,13 @@
+﻿namespace csharp_project_begin.Threatning;
+
+public class Ex4
+{
+    static async Task Main(String[] args)
+    {
+        Console.WriteLine("Begin to work");
+        await Task.Delay(2000);
+        Console.WriteLine("End of work");
+    }
+
+    static async
+}
